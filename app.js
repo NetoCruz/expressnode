2f4f4fc2ed820702294b0fdc6146d6574668da41
unknown <galaxy.net0710@gmail.com>
2020-08-12 18:46:51 -0500

    working

--- app.js
+++ app.js
@@ -5,5 +5,15 @@ const app =express();
 app.get('/',(req, res)=>{
   res.send('Hello World');
 });
+app.get('/home',(req,res)=>{
+  res.send('Bienvenido al home')
+})
+
+app.get('/home/:name/:age',(req,res)=>{
+    console.log(req.params);
+    console.log(req.query)
+    //res.send('ejemplo con route params')
+    res.send(req.params.name + ':' + req.params.age)
+  })
 
 app.listen(3000);
