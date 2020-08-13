72f9a77b1d0dacf9593cca5d2f5ab928479b1be7
unknown <galaxy.net0710@gmail.com>
2020-08-12 19:14:29 -0500

    Http Post Request with Express and Body Parser Module

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
-
+const path = require('path')
 const app =express();
+const bodyParser= require('body-parser')
+
+
+app.use('/public', express.static(path.join(__dirname,'static')));
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/',(req, res)=>{
-  res.send('Hello World');
+  res.sendFile(path.join(__dirname,'static','index.html'))
 });
+
+app.post('/',(req,res)=>{
+    console.log(req.body)
+    //data base work here
+    res.send('successfully posted data')
+})
 
 app.listen(3000);
